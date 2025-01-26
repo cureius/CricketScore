98dd34bbb7cfe15062f2110aad96e6e9773722e2
souraj-GDL <souraj.x.pal@godeskless.com>
2025-01-27 00:01:05 +0530

    changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,6 @@ module.exports = {
   root: true,
   extends: '@react-native',
   rules: {
-    // '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
   },
 };
